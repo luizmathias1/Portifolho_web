73437e47779718e3e5d53da1ab68a7aa0afb1bdb
luizmathias <luizmathiasrivabem@gmail.com>
2024-09-10 13:48:43 -0300

    reformando o site para virar um portifolho e adicionando funcionalidade aos botões do header

---
+++ index.js
@@ -0,0 +1,17 @@
+function IrInicio() {
+    const destino = document.getElementById("Inicio");
+    destino.scrollIntoView({ behavior: "smooth" });
+}
+
+
+function IrProjetos() {
+    const destino = document.getElementById("Projetos");
+    destino.scrollIntoView({ behavior: "smooth" });
+}
+
+function IrContato() {
+    const destino = document.getElementById("Contato");
+    destino.scrollIntoView({ behavior: "smooth" });
+}
+
+
